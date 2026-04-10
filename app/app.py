2fd702b8f9e13191d2a25081205c7795db2cc359
jotaaga1 <jhfs4580@gmail.com>
2026-04-10 09:53:12 -0300

    sdf

--- app/app.py
+++ app/app.py
@@ -3,10 +3,13 @@ from models import db, User, Category, Transaction, AuditLog
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///finance.db'
-db.int_app(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
 
 with app.app_context():
     db.create_all()
+
 
 @app.route('/transaction', methods=['POST'])
 def add_transaction():
@@ -16,18 +19,24 @@ def add_transaction():
         return jsonify({"error": "O valor deve ser positivo"}), 400
 
     new_trans = Transaction(
-        descripetion=data['descripition'],
-        amount=data['user_id'],
+        description=data['description'],
+        amount=data['amount'],
         user_id=data['user_id'],
         category_id=data['category_id']
     )
 
     db.session.add(new_trans)
-    log = AuditLog(action=f"Lancamento de {data['amount']} criado", user_id=data['user_id'])
+
+    log = AuditLog(
+        action=f"Lançamento de {data['amount']} criado",
+        user_id=data['user_id']
+    )
     db.session.add(log)
 
     db.session.commit()
-    return jsonify({"message": "Lançamento realizado com sucesso!"}) , 201
+
+    return jsonify({"message": "Lançamento realizado com sucesso!"}), 201
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/models.py
+++ app/models.py
@@ -6,26 +6,29 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.Sring(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     categories = db.relationship('Category', backref='owner', lazy=True)
     transactions = db.relationship('Transaction', backref='owner', lazy=True)
+
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     type = db.Column(db.String(10), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeingKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
 
 class Transaction(db.Model):
-    id = db.Column(id.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(200), nullable=False)
     amount = db.Column(db.Float, nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeingKey('user.id'), nullable=False)
-    category_id = db.Column(db.Integer, db.ForeingKey('category.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+
 
 class AuditLog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     action = db.Column(db.String(200))
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeingKey('user_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
